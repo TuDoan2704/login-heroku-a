c14e7f23a8ccbffa921212ab820f2eb551fe988e
TuDoan2704 <doantund2812@gmail.com>
2021-11-01 08:21:14 +0700

    aa

---
+++ src/main/java/com/loginspringboot/Loginspringboot20Application.java
@@ -0,0 +1,13 @@
+package com.loginspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Loginspringboot20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Loginspringboot20Application.class, args);
+	}
+
+}
